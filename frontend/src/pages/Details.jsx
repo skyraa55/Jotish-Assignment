2c1714616d39b0c86907ac4b2f53c811f0c72004
gowthii <ekambaramgowthami@gmail.com>
2026-03-16 22:08:42 +0530

    Improve UI of Employee List and Employee Details pages

--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -1,89 +1,109 @@
 import { useEffect, useRef } from "react";
 import { useNavigate, useParams } from "react-router-dom"
 
-export default function Details(){
+export default function Details() {
     const { id } = useParams();
     const videoRef = useRef(null);
     const canvasRef = useRef(null);
     const drawingRef = useRef(false);
     const drawCanvasRef = useRef(null);
     const navigate = useNavigate();
-  
-    useEffect(()=>{
+
+    useEffect(() => {
         startCamera();
-    },[]);
-    const startCamera = async ()=>{
+    }, []);
+    const startCamera = async () => {
         const stream = await navigator.mediaDevices.getUserMedia({
-            video:true
+            video: true
         });
         videoRef.current.srcObject = stream;
     }
-    const capturePhoto = ()=>{
+    const capturePhoto = () => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext("2d");
         canvas.width = 400;
         canvas.height = 300;
-        ctx.drawImage(videoRef.current,0,0,400,300);
+        ctx.drawImage(videoRef.current, 0, 0, 400, 300);
 
     };
-    const startDrawing = (e)=>{
+    const startDrawing = (e) => {
         drawingRef.current = true;
         const drawCanvas = drawCanvasRef.current;
         const ctx = drawCanvas.getContext("2d");
         const rect = drawCanvas.getBoundingClientRect();
         ctx.beginPath();
-        ctx.moveTo(e.clientX-rect.left,e.clientY-rect.top);
+        ctx.moveTo(e.clientX - rect.left, e.clientY - rect.top);
     }
-    const stopDrawing = ()=>{
+    const stopDrawing = () => {
         drawingRef.current = false;
     }
-      const draw = (e)=>{
-        if(!drawingRef.current) return;
+    const draw = (e) => {
+        if (!drawingRef.current) return;
         const drawCanvas = drawCanvasRef.current;
         const ctx = drawCanvas.getContext("2d");
         const rect = drawCanvas.getBoundingClientRect();
         ctx.lineWidth = 2;
         ctx.lineCap = "round";
-        ctx.lineTo(e.clientX-rect.left,e.clientY-rect.top);
+        ctx.lineTo(e.clientX - rect.left, e.clientY - rect.top);
         ctx.stroke();
 
     }
-    const mergePhotoAndSignature = ()=>{
+    const mergePhotoAndSignature = () => {
         const photoCanvas = canvasRef.current;
         const signatureCanvas = drawCanvasRef.current;
         const finalCanvas = document.createElement('canvas');
         const ctx = finalCanvas.getContext("2d");
-        const width = 400;
-        const photoHeight = 300;
+        const width = 100;
+        const photoHeight = 100;
         const signHeight = 60;
-        finalCanvas.width=width;
-        finalCanvas.height=photoHeight+signHeight;
-        ctx.drawImage(photoCanvas,0,0,width,photoHeight);
-        ctx.drawImage(signatureCanvas,0,photoHeight,width,signHeight);
+        finalCanvas.width = width;
+        finalCanvas.height = photoHeight + signHeight;
+        ctx.drawImage(photoCanvas, 0, 0, width, photoHeight);
+        ctx.drawImage(signatureCanvas, 0, photoHeight, width, signHeight);
 
         const finalImage = finalCanvas.toDataURL("image/png");
-        localStorage.setItem("mergedImage",finalImage);
+        localStorage.setItem("mergedImage", finalImage);
         const link = document.createElement("a");
         link.href = finalImage;
-        link.download="employee_photo_signature.png";
+        link.download = "employee_photo_signature.png";
         link.click();
         navigate("/analytics");
 
     }
     return (
-        <div>
-            <h1 className="text-3xl font-semibold mb-2">Employee Id: {id}</h1>
-            <video ref={videoRef} autoPlay width={400} />
-            <br />
-            <button className="px-4 py-2 rounded-lg bg-green-500 text-white" onClick={capturePhoto}>cature photo</button>
-            <br />
-            <canvas ref={canvasRef} style={{ border:"1px solid black "}} />
-            <h2 className="px-4 py-2 rounded-lg bg-green-500 text-white" >Signature</h2>
-            <canvas ref={drawCanvasRef} width="400" height="60" style={{ border:"1px solid black "}} onMouseDown={startDrawing} onMouseLeave={stopDrawing} onMouseUp={stopDrawing} onMouseMove={draw} />
-            <br />
-            <button className="px-4 py-2 rounded-lg bg-green-500 text-white" onClick={mergePhotoAndSignature} >
-                photo+sign 
-            </button>
+        <div className="flex justify-center items-center h-screen bg-gray-100/50">
+            {/* <h1 className="text-3xl font-semibold mb-2">Employee Id: {id}</h1> */}
+            <div className="bg-white rounded-2xl p-4 border border-gray-200 shadow-2xl">
+                <div className="flex gap-2">
+                    <video ref={videoRef} autoPlay width={300} height={200} className="rounded-xl" />
+                        <canvas ref={drawCanvasRef} width="300" height={200} style={{ border: "1px solid #e5e7eb",borderRadius:"10px" }} onMouseDown={startDrawing} onMouseLeave={stopDrawing} onMouseUp={stopDrawing} onMouseMove={draw} />
+                          {/* <h2 className="px-3 py-3 rounded-lg bg-green-500 text-white" >Signature</h2> */}
+                </div>
+
+                <br />
+                <div className="flex justify-center items-center">
+                    <button className="px-4 py-2 rounded-lg bg-[#635db8] text-white shadow-xl" onClick={capturePhoto}>cature photo</button>
+
+                </div>
+                
+                <br />
+                <div className="flex justify-center items-center">
+                     <canvas ref={canvasRef} style={{ border: "1px solid #e5e7eb",borderRadius:"10px" }} width={300} height={200} />
+
+                </div>
+               
+
+                <br />
+                <div className="flex justify-center items-center">
+                    <button className="px-4 py-2 rounded-lg bg-[#2a996b] text-white shadow-xl" onClick={mergePhotoAndSignature} >
+                    photo+sign
+                </button>
+
+                </div>
+                
+
+            </div>
+
         </div>
     );
 }
--- frontend/src/pages/EmployeeList.jsx
+++ frontend/src/pages/EmployeeList.jsx
@@ -1,14 +1,17 @@
 import axios from "axios";
 import { React, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Icon from "../svgs/Icon";
+import User from "../svgs/User";
 
 export default function EmployeeList() {
     const navigate = useNavigate();
     const [employees, setEmployees] = useState([]);
     const [scrollTop, setScrollTop] = useState(0);
-    const containerHeight = 600;
-    const rowHeight = 50;
+    const containerHeight = 550;
+    const rowHeight = 60;
     const buffer = 5;
+    const totalEmployees = employees.length;
     useEffect(() => {
         const cachedEmployeee = localStorage.getItem("employees");
         if (cachedEmployeee) {
@@ -19,7 +22,14 @@ export default function EmployeeList() {
         }
 
     }, []);
-
+    const cityCounts = employees.reduce((acc, emp) => {
+        const city = emp[2];
+        if (!acc[city]) {
+            acc[city] = 0;
+        }
+        acc[city]++;
+        return acc;
+    }, {});
     const fetchEmployees = async () => {
         const res = await axios.post("https://backend.jotish.in/backend_dev/gettabledata.php", {
             username: "test",
@@ -37,50 +47,136 @@ export default function EmployeeList() {
     const totalHeight = employees.length * rowHeight;
     const offsetY = startIndex * rowHeight;
     return (
-        <div className="p-6">
-            <h1 className="text-3xl font-bold mb-4">Employee List </h1>
-            <div style={{
-                height: containerHeight,
-                overflowY: "auto",
-                border: "1px solid #ccc"
-            }}
-                onScroll={(e) => setScrollTop(e.target.scrollTop)}
-            >
+        <div className="h-screen w-full bg-white overflow-hidden ">
+            <div className="h-20 bg-[#635db8] rounded-b-lg text-white text-3xl font-semibold flex items-center p-6 pl-12 pr-12">
+                <div className="flex justify-between items-center w-full">
+                    <div className="flex gap-4  items-center">
+                        <Icon />
+                        <div>Employee List</div>
+                    </div>
+                    <User />
+
+                </div>
+
+            </div>
+            <div className="w-full px-92 py-8 grid grid-cols-4 gap-6">
+                <div className="bg-[#F4F4F5] border border-[#DBDBDC] shadow-xl rounded-xl pt-4 pb-4 flex flex-col items-center gap-3 text-[#252437]">
+                    <div className="flex items-center gap-2">
+                        <p className="text-[#252437] text-xl font-semibold">Total Employees</p>
+                    </div>
+                    <h2 className="text-2xl text-[#252437] font-semibold">{totalEmployees}</h2>
+                </div>
+
+                <div className="bg-[#F4F4F5] border border-[#DBDBDC] shadow-xl rounded-xl pt-4 pb-4 flex flex-col items-center gap-3">
+                    <div className="flex items-center gap-2">
+                        <p className="text-[#252437] text-xl font-semibold">Edinburgh</p>
+                    </div>
+                    <h2 className="text-2xl text-[#252437] font-semibold">{cityCounts["Edinburgh"] || 0}</h2>
+                </div>
+                <div className="bg-[#F4F4F5] border border-[#DBDBDC] shadow-xl rounded-xl pt-4 pb-4 flex flex-col items-center gap-2">
+                    <div className="flex items-center gap-2">
+                        <p className="text-[#252437] text-xl font-semibold">Tokyo</p>
+                    </div>
+                    <h2 className="text-2xl text-[#252437] font-semibold">{cityCounts["Tokyo"] || 0}</h2>
+                </div>
+                <div className="bg-[#F4F4F5] border border-[#DBDBDC] shadow-xl rounded-xl pt-4 pb-4  flex flex-col items-center gap-2">
+                    <div className="flex items-center gap-2">
+                        <p className="text-[#252437] text-xl font-semibold">San Francisco</p>
+                    </div>
+                    <h2 className="text-2xl text-[#252437] font-semibold">{cityCounts["San Francisco"] || 0}</h2>
+                </div>
+            </div>
+            <div className="w-full px-100 py-8 grid grid-cols-3 gap-6 pl-120 -translate-y-10" >
+                <div className="bg-[#F4F4F5] border border-[#DBDBDC] shadow-xl rounded-xl pt-4 pb-4  flex flex-col items-center gap-2 w-70 h-28">
+                    <div className="flex items-center gap-2">
+                        <p className="text-[#252437] text-xl font-semibold">New York</p>
+                    </div>
+                    <h2 className="text-2xl text-[#252437] font-semibold">{cityCounts["New York"] || 0}</h2>
+                </div>
+                <div className="bg-[#F4F4F5] border border-[#DBDBDC] shadow-xl rounded-xl pt-4 pb-4  flex flex-col items-center gap-2  w-70 h-28">
+                    <div className="flex items-center gap-2">
+                        <p className="text-[#252437] text-xl font-semibold">London</p>
+                    </div>
+                    <h2 className="text-2xl text-[#252437] font-semibold">{cityCounts["London"] || 0}</h2>
+                </div>
+                <div className="bg-[#F4F4F5] border border-[#DBDBDC] shadow-xl rounded-xl pt-4 pb-4  flex flex-col items-center gap-2  w-70 h-28">
+                    <div className="flex items-center gap-2">
+                        <p className="text-[#252437] text-xl font-semibold">Singapore</p>
+                    </div>
+                    <h2 className="text-2xl text-[#252437] font-semibold">{cityCounts["Singapore"] || 0}</h2>
+                </div>
+
+            </div>
+            <div className=" pl-8 pr-8">
                 <div
                     style={{
-                        height: totalHeight,
-                        position: "relative"
+                        display: "flex",
+                        padding: "16px",
+                        fontWeight: "bold",
+                        borderBottom: "2px solid #ccc",
+                        height: rowHeight,
+                        alignItems: "center",
+                        backgroundColor: "#2a996b",
+                        borderRadius:"10px",
+                        color:"white"
+                       
                     }}
+                >
+                    <div style={{ width: "80px",fontSize:"20px" }}>S.No</div>
+                    <div style={{ flex: 1,fontSize:"20px" }}>Name</div>
+                    <div style={{ flex: 1,fontSize:"20px" }}>Position</div>
+                    <div style={{ flex: 1,fontSize:"20px" }}>City</div>
+                    <div style={{ flex: 1 ,fontSize:"20px"}}>Extn.</div>
+                    <div style={{ flex: 1 ,fontSize:"20px"}}>Start Date</div>
+                    <div style={{ flex: 1 ,fontSize:"20px"}}>Salary</div>
+                </div>
+                <div style={{
+                    height: containerHeight,
+                    overflowY: "auto",
+                    border: "1px solid #ccc",
+                    borderRadius: "10px"
+
+                }}
+                    className="bg-white"
+                    onScroll={(e) => setScrollTop(e.target.scrollTop)}
                 >
                     <div
                         style={{
-                            transform: `translateY(${offsetY}px)`
-                        }}>
-                        {visibleData.map((emp, index) => {
-                            return (
-                                <div key={startIndex + index}
-                                    onClick={() => navigate(`/details/${startIndex + index}`)}
-                                    style={{
-                                        height: rowHeight,
-                                        borderBottom: "1px solid #eee",
-                                        display: "flex",
-                                        alignItems: "center",
-                                        padding: "0 16px"
-                                    }}
-                                >
-                                    <div style={{ width: "80px" }}>{startIndex + index + 1}</div>
-                                    <div style={{ flex: 1 }}>{emp[0]}</div>
-                                    <div style={{ flex: 1 }}>{emp[1]}</div>
-                                    <div style={{ flex: 1 }}>{emp[2]}</div>
-                                    <div style={{ flex: 1 }}>{emp[3]}</div>
-                                    <div style={{ flex: 1 }}>{emp[4]}</div>
-                                    <div style={{ flex: 1 }}>{emp[5]}</div>
+                            height: totalHeight,
+                            position: "relative"
+                        }}
+                    >
+                        <div
+                            style={{
+                                transform: `translateY(${offsetY}px)`
+                            }}>
+                            {visibleData.map((emp, index) => {
+                                return (
+                                    <div key={startIndex + index}
+                                        onClick={() => navigate(`/details/${startIndex + index}`)}
+                                        style={{
+                                            height: rowHeight,
+                                            borderBottom: "1px solid #eee",
+                                            display: "flex",
+                                            alignItems: "center",
+                                            padding: "0 16px"
+                                        }}
+                                    >
+                                        <div style={{ width: "80px" }}>{startIndex + index + 1}</div>
+                                        <div style={{ flex: 1, fontSize: "18px" }}>{emp[0]}</div>
+                                        <div style={{ flex: 1, fontSize: "18px" }}>{emp[1]}</div>
+                                        <div style={{ flex: 1, fontSize: "18px" }}>{emp[2]}</div>
+                                        <div style={{ flex: 1, fontSize: "18px" }}>{emp[3]}</div>
+                                        <div style={{ flex: 1, fontSize: "18px" }}>{emp[4]}</div>
+                                        <div style={{ flex: 1, fontSize: "18px" }}>{emp[5]}</div>
 
-                                </div>
-                            )
+                                    </div>
+                                )
 
 
-                        })}
+                            })}
+
+                        </div>
 
                     </div>
 
@@ -89,5 +185,6 @@ export default function EmployeeList() {
             </div>
 
         </div>
+
     )
 }
